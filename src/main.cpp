ba4a94fca45c3be7a7582b886c1f9749b0dc8229
Jean-Francois Duval <jfduval@aqra.ca>
2018-01-15 15:08:33 -0500

    Cleaned some old code. I can now send an ActPack command. No read yet - WiP

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,40 @@
 #include "flexsea_config.h"
 #include "flexsea_interface.h"
 #include "serial.h"
+#include "cmd-ActPack.h"
 
 #include "flexsea_user_structs.h"
 
 using namespace std;
 
-//Original:
-void genAndSendFlexSEAPacket(uint8_t arrLen)
-{
-	static uint8_t packetIndex = 0;
-	packetIndex++;
-	uint16_t nb = 0;
-	uint8_t commStr[100];
+uint8_t packetIndex = 0;
+uint16_t nb = 0;
+uint8_t commStr[100];
 
-	//Prepare and send command:
-	tx_cmd_tools_comm_test_w(TX_N_DEFAULT, 1, arrLen, packetIndex, 0);
-	pack(P_AND_S_DEFAULT, FLEXSEA_MANAGE_1, NULL, &nb, commStr);
-	flexsea_serial_transmit(nb, commStr, 0);
-}
+//ActPack variables:
+uint8_t controller = CTRL_NONE;
+int32_t setpoint = 0;
+uint8_t setGains = KEEP;
+int16_t g0 = 0;
+int16_t g1 = 0;
+int16_t g2 = 0;
+int16_t g3 = 0;
+uint8_t syst = 0;
+
 
 //Testing the simplified approach:
 void genAndSendFlexSEAPacket_simplified(uint8_t arrLen)
 {
-	static uint8_t packetIndex = 0;
 	packetIndex++;
-	uint16_t nb = 0;
-	uint8_t commStr[100];
 
 	//Prepare and send command:
 	ptx_cmd_tools_comm_test_w(FLEXSEA_MANAGE_1, &nb, commStr, 1, arrLen, packetIndex, 0);
+	flexsea_serial_transmit(nb, commStr, 0);
+}
+
+void requestReadActPack(uint8_t offset)
+{
+	ptx_cmd_actpack_rw(FLEXSEA_MANAGE_1, &nb, commStr, offset, controller, setpoint, setGains, g0, g1, g2, g3, syst);
 	flexsea_serial_transmit(nb, commStr, 0);
 }
 
@@ -46,20 +51,15 @@ int main()
 	//Prepare FlexSEA stack:
 	initFlexSEAStack_minimalist(FLEXSEA_PLAN_1);
 
-	//Test: can we R/W to variables?
-	cout << "rigid1.mn.accel.x:" << rigid1.mn.accel.x << endl;
-	rigid1.mn.accel.x = 10;
-	cout << "rigid1.mn.accel.x:" << rigid1.mn.accel.x << endl;
-	//(Yes)
-
 	//Serial driver test:
-	flexsea_serial_open(1,0);
+	flexsea_serial_open(1, 0);
 
 	//Send data for Xs, 100Hz:
 	for(int i = 0; i < 10; i++)
 	{
 		printf("Sending FlexSEA packet #%i...\n", i);
-		genAndSendFlexSEAPacket_simplified(10);
+		//genAndSendFlexSEAPacket_simplified(10);
+		requestReadActPack(0);
 		Sleep(10);
 	}
 
--- src/serial.c
+++ src/serial.c
@@ -49,98 +49,23 @@ int flexsea_serial_open(unsigned int tries, unsigned int delay)
 		printf("\nOpening serial port successful\n");
 
 	return 1;
-//	unsigned int cnt = 0;
-//
-//	do
-//	{
-//		fd = open(device, O_RDWR | O_NOCTTY);
-//		cnt++;
-//		if(cnt >= tries)
-//			break;
-//
-//		usleep(delay);
-//	}while(fd < 0);
-//
-//
-//	if (fd < 0)
-//	{
-//		printf("Tried %i times, couldn't open %s.\n", cnt, device);
-//		return 1;
-//	}
-//	else
-//	{
-//		printf("Successfully opened %s.\n", device);
-//
-//		/* Set Baud Rate */
-//		cfsetospeed (&tty, (speed_t)B57600);
-//		cfsetispeed (&tty, (speed_t)B57600);
-//
-//		/* Setting other Port Stuff */
-//		tty.c_cflag	 &=  ~PARENB;				// Make 8n1
-//		tty.c_cflag	 &=  ~CSTOPB;
-//		tty.c_cflag	 &=  ~CSIZE;
-//		tty.c_cflag	 |=  CS8;
-//
-//		tty.c_cflag	 &=  ~CRTSCTS;		   		// no flow control
-//		tty.c_cc[VMIN]   =  1;				  	// read doesn't block
-//		tty.c_cc[VTIME]  =  5;				  	// 0.5 seconds read timeout
-//		tty.c_cflag	 |=  CREAD | CLOCAL;	 	// turn on READ & ignore ctrl lines
-//
-//		/* Make raw */
-//		cfmakeraw(&tty);
-//
-//		/* Flush Port, then applies attributes */
-//		tcflush(fd, TCIFLUSH);
-//		if ( tcsetattr (fd, TCSANOW, &tty ) != 0)
-//		{
-//			printf("Error from tcsetattr.\n");
-//			return 2;
-//		}
-//
-//		return 0;
-//	}
 }
 	
 //Serial port test: sends a single char
 void flexsea_serial_putc(char *bytes_to_write)
 {
-//	if(fd < 0)
-//		return;
-//
-//	//Valid port
-//	if(write(fd, bytes_to_write, 1))
-//		printf("Wrote a byte\n");
-//	else
-//		printf("Failed.\n");
 
 }
 
 //Closes the SPI port
 void flexsea_serial_close(void)
 {
-//	close(fd);
-
 	CloseHandle(hComm);//Closing the Serial Port
 }
 
 void flexsea_serial_transmit(char bytes_to_send, unsigned char *serial_tx_data, unsigned char verbal)
 {
 	BOOL Status;
-
-//	if(fd < 0)
-//		return;
-//
-//	//Valid port
-//	if(write(fd, (char *)serial_tx_data, bytes_to_send))
-//	{
-//		//printf("Wrote a string\n");
-//	}
-//	else
-//	{
-//		printf("Failed.\n");
-//	}
-
-	//printf("serialdata: %s\n", serial_tx_data);
 
 	char   lpBuffer[100] = "A";		       // lpBuffer should be  char or byte array, otherwise write will fail
 	memcpy(lpBuffer, serial_tx_data, bytes_to_send);
