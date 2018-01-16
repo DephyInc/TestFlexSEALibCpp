ae43149d7ae5cb6440777d8aecc53c4cd20557df
Jean-Francois Duval <jfduval@aqra.ca>
2018-01-16 16:24:28 -0500

    Added console input to select demo

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,43 @@
 
 using namespace std;
 
+uint8_t activeDemo = 0;
+
+void printMenu(void)
+{
+	cout << "What demo would you like to try?" << endl << endl;
+	cout << "[1] Read-Only: no actuator, sensors only." << endl;
+	cout << "[2] Open-Speed: PWM will ramp up, then down, then up..." << endl;
+	cout << "[3] Position-1: Motor will hold its current position." << endl;
+	cout << "[4] Position-2: Motor will toggle between two positions." << endl;
+	cout << "[q] Quit program." << endl << endl;
+	//ToDo: expand that list
+}
+
+char userSelection(void)
+{
+	char c = 0;
+	cout << "Please enter your selection: ";
+	cin >> c;
+
+	if(c >= '1' && c <= '4')
+	{
+		//Valid demo:
+		activeDemo = (int)(c - '0');
+	}
+	else if(c == 'q')
+	{
+		activeDemo = 0;
+	}
+	else
+	{
+		cout << "Invalid selection, please try again." << endl;
+		userSelection();
+	}
+
+	return activeDemo;
+}
+
 int main()
 {
 	cout << "Demo code - C++ project with FlexSEA-Stack DLL" << endl;
@@ -28,12 +65,27 @@ int main()
 	//Open port (hardcoded):
 	flexsea_serial_open(1, 0);
 
+	printMenu();
+	userSelection();
+
 	sleep(1);
 
 	//Infinite loop - break w/ Ctrl+c
-	while(1)
+	while(activeDemo != 0)
 	{
-		readActPack(0,0,10);
+		switch(activeDemo)
+		{
+			case 1:
+				readActPack(0,0,10);
+				break;
+			case 2:
+				cout << "Not programmed!" << endl;
+				break;
+			default:
+				cout << "Not programmed!" << endl;
+				break;
+		}
+
 		Sleep(5);
 	}
 
